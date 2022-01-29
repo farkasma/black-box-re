9c2bd40e1bf777b1c95bd1a54e119542262c2ca2
farkasma <farkasmrton@gmail.com>
2022-01-29 03:20:26 +0100

    v0.2
    -Added a function for checking a solution
    -Added a function for adding or removing children in one go
    -Reworked the Tile class for added functionality
    -Refactored a whole lot of things to accomodate the checking algorithm

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 class Tile {
     element;
-    ball = false
+    errorElement;
+    value = "";
+    checkingValue = "";
+    ball = false;
+    guess = false;
+    pair = {
+        r: 0,
+        c: 0
+    }
+    sync() {
+        this.element.innerText = this.value
+        if (this.errorElement != null) {
+            this.element.appendChild(this.errorElement)
+        }
+    }
 }
 
 var field = document.getElementById("field")
@@ -8,6 +22,7 @@ var size = 5
 var balls = 3
 var board = []
 var exits = 1
+var checkingExits = 1
 
 initialize()
 
@@ -22,7 +37,7 @@ function initialize() {
                 tempelem.classList.add("corner")
             } else if (i == 0 || i == size + 1 || j == 0 || j == size + 1) {
                 tempelem.classList.add("edge")
-                tempelem.setAttribute("onclick", "laser(" + i + ", " + j + ")")
+                tempelem.setAttribute("onclick", "laser(" + i + ", " + j + ", false)")
             } else {
                 tempelem.classList.add("inner")
                 tempelem.setAttribute("onclick", "reveal(" + i + ", " + j + ")")
@@ -36,23 +51,22 @@ function initialize() {
             field.appendChild(tempelem)
         }
     }
-    var i = balls
-    while (i > 0) {
+    var a = balls
+    while (a > 0) {
         var x, y
         x = Math.floor(Math.random() * 5 + 1)
         y = Math.floor(Math.random() * 5 + 1)
         if (board[x][y].ball === false) {
             board[x][y].ball = true
-            i--
+            a--
         }
     }
 }
 
-function laser(r, c) {
+function laser(r, c, checking) {
     var startr = r
     var startc = c
     var start = board[r][c]
-    if (start.element.innerHTML != "") return
     var x = 0, y = 0 //x: row delta, y: column delta
     switch (r) {
         case 0: x = 1; break;
@@ -63,12 +77,23 @@ function laser(r, c) {
                 case 6: y = -1; break;
             }
     }
+    var read, write, exit
+    if (checking) {
+        read = "guess"
+        write = "checkingValue"
+        exit = "checkingExits"
+    } else {
+        if (start.value != "") return
+        read = "ball"
+        write = "value"
+        exit = "exits"
+    }
     var state = 0
     while (state != 12) {
         switch (state) {
             case 0:
-                if (board[r + x][c + y].ball === true) {
-                    start.element.innerHTML = "H"
+                if (board[r + x][c + y][read] === true) {
+                    start[write] = "H"
                     state = 12
                     break
                 }
@@ -76,24 +101,24 @@ function laser(r, c) {
                 break
             case 1:
                 if (x == 0) {
-                    if (board[r + 1][c + y].ball === true) {
-                        start.element.innerHTML = "R"
+                    if (board[r + 1][c + y][read] === true) {
+                        start[write] = "R"
                         state = 12
                         break
                     }
-                    if (board[r - 1][c + y].ball === true) {
-                        start.element.innerHTML = "R"
+                    if (board[r - 1][c + y][read] === true) {
+                        start[write] = "R"
                         state = 12
                         break
                     }
                 } else { //y = 0
-                    if (board[r + x][c + 1].ball === true) {
-                        start.element.innerHTML = "R"
+                    if (board[r + x][c + 1][read] === true) {
+                        start[write] = "R"
                         state = 12
                         break
                     }
-                    if (board[r + x][c - 1].ball === true) {
-                        start.element.innerHTML = "R"
+                    if (board[r + x][c - 1][read] === true) {
+                        start[write] = "R"
                         state = 12
                         break
                     }
@@ -110,8 +135,8 @@ function laser(r, c) {
                 }
                 break
             case 3:
-                if (board[r + x][c + y].ball === true) {
-                    start.element.innerHTML = "H"
+                if (board[r + x][c + y][read] === true) {
+                    start[write] = "H"
                     state = 12
                     break
                 }
@@ -125,14 +150,14 @@ function laser(r, c) {
                 }
                 break
             case 5:
-                if (board[r + 1][c + y].ball === true) {
+                if (board[r + 1][c + y][read] === true) {
                     state = 6
                 } else {
                     state = 7
                 }
                 break
             case 6:
-                if (board[r - 1][c + y].ball === true) {
+                if (board[r - 1][c + y][read] === true) {
                     y = -y
                 } else {
                     y = 0
@@ -143,7 +168,7 @@ function laser(r, c) {
                 state = 2
                 break
             case 7:
-                if (board[r - 1][c + y].ball === true) {
+                if (board[r - 1][c + y][read] === true) {
                     y = 0
                     x = 1
                 }
@@ -152,14 +177,14 @@ function laser(r, c) {
                 state = 2
                 break
             case 8:
-                if (board[r + x][c + 1].ball === true) {
+                if (board[r + x][c + 1][read] === true) {
                     state = 9
                 } else {
                     state = 10
                 }
                 break
             case 9:
-                if (board[r + x][c - 1].ball === true) {
+                if (board[r + x][c - 1][read] === true) {
                     x = -x
                 } else {
                     x = 0
@@ -170,7 +195,7 @@ function laser(r, c) {
                 state = 2
                 break
             case 10:
-                if (board[r + x][c - 1].ball === true) {
+                if (board[r + x][c - 1][read] === true) {
                     x = 0
                     y = 1
                 }
@@ -180,18 +205,26 @@ function laser(r, c) {
                 break
             case 11:
                 if (startr == r && startc == c) {
-                    board[r][c].element.innerHTML = "R"
+                    board[r][c][write] = "R"
                     state = 12
                     break
                 } else {
-                    start.element.innerHTML = exits
-                    board[r][c].element.innerHTML = exits
-                    exits++
+                    start[write] = window[exit] //not very clean, but will do
+                    board[r][c][write] = window[exit]
+                    if (!checking) {
+                        start.pair.r = r
+                        start.pair.c = c
+                        board[r][c].pair.r = startr
+                        board[r][c].pair.c = startc    
+                    }
+                    window[exit]++
                     state = 12
                     break
                 }
         }
     }
+    board[r][c].sync()
+    start.sync()
 }
 
 function reveal(r, c) {
@@ -201,10 +234,81 @@ function reveal(r, c) {
 }
 
 function flag(r, c) {
-    if (board[r][c].element.firstChild == null) {
-        board[r][c].element.appendChild(document.createElement("div"))
-        board[r][c].element.firstChild.classList.add("ball")
-    } else {
-        board[r][c].element.removeChild(board[r][c].element.firstChild)
+    toggleChild(board[r][c], "ball")
+}
+
+function checkBoard() {
+    for (let i = 0; i < 7; i += 6) {
+        for (let j = 1; j < 6; j++) {
+            laser(i, j, true)
+            checkTile(board[i][j])
+            laser(j, i , true)
+            checkTile(board[j][i])
+        }
     }
+}
+
+function checkTile(tile) {
+    if (typeof(tile.value) == "string") {
+        if (tile.value != tile.checkingValue) {
+            if (tile.errorElement == null) {
+                toggleChild(tile, "error")
+            }
+        } else {
+            if (tile.errorElement != null) {
+                toggleChild(tile, "error")
+            }
+        }
+    } else {
+        if (typeof(tile.checkingValue) == "number") {
+            if (board[tile.pair.r][tile.pair.c].checkingValue != tile.checkingValue) { //slightly less ewww
+                if (tile.errorElement == null) {
+                    toggleChild(tile, "error")
+                }
+            } else {
+                if (tile.errorElement != null) {
+                    toggleChild(tile, "error")
+                }
+            }    
+        } else {
+            if (tile.errorElement == null) {
+                toggleChild(tile, "error")
+            }
+        }
+    }
+}
+
+function revealAllEdges() {
+    for (let i = 0; i < 7; i += 6) {
+        for (let j = 1; j < 6; j++) {
+            laser(i, j, false)
+            laser(j, i, false)
+        }
+    }
+}
+
+function toggleChild(tile, type) {
+    switch (type) {
+        case "error":
+            if (tile.errorElement == null) {
+                let temp = document.createElement("div")
+                temp.classList.add("error")
+                tile.errorElement = temp
+                tile.element.appendChild(temp)
+            } else {
+                tile.errorElement.remove()
+                tile.errorElement = null
+            }
+            break
+        case "ball":
+            if (tile.element.firstChild == null) {
+                let temp = document.createElement("div")
+                temp.classList.add("ball")
+                tile.element.appendChild(temp)
+                tile.guess = true
+            } else {
+                tile.element.removeChild(tile.element.firstChild)
+                tile.guess = false
+            }
+        }    
 }
